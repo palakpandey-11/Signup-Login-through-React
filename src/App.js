faf6d78e200ae08c48cddf9efc9c09223d80edae
palak pandey <palakpandey9608@gmail.com>
2025-06-08 18:57:59 +0530

    changes done in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login.js";
 import SignUp from "./components/SignUp.js";
 import Dashboard from "./components/Dashboard.js";
